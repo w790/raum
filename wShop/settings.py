c7143b4f9ce94be34ebd6bba30036eead2773dae
pasha <pasha_w790@bk.ru>
2025-12-25 05:18:22 +0700

    config: Update Celery Redis host to `redis` and adjust Nginx SSL ciphers.

--- wShop/settings.py
+++ wShop/settings.py
@@ -158,8 +158,8 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
+CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://redis:6379/0')
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
